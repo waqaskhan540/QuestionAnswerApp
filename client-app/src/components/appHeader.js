5c96b9fde1dc5eccb83f98bf8bf10317e38c61bd
waqas <wasikhan540@gmail.com>
2019-09-06 13:21:04 +0500

    ui changes

--- client-app/src/components/appHeader.js
+++ client-app/src/components/appHeader.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { Input, Menu } from "semantic-ui-react";
-import {withRouter} from "react-router-dom"
+import { Input, Menu, Icon } from "semantic-ui-react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
 class AppHeader extends React.Component {
   state = { activeItem: "home" };
 
   handleItemClick = (e, { name }) => {
     this.setState({ activeItem: name });
-    this.props.history.push("/"+name)
-  }
+    this.props.history.push("/" + name);
+  };
 
   render() {
     const { activeItem } = this.state;
+    const { user } = this.props;
+
     return (
       <div>
         <Menu pointing>
@@ -24,21 +27,35 @@ class AppHeader extends React.Component {
             name="questions"
             active={activeItem === "questions"}
             onClick={this.handleItemClick}
-          />        
+          />
           <Menu.Menu position="right">
             <Menu.Item>
               <Input icon="search" placeholder="Search..." />
             </Menu.Item>
-            <Menu.Item
-              name="login"
-              active={activeItem === "login"}
-              onClick={this.handleItemClick}
-            />
-             <Menu.Item
-              name="register"
-              active={activeItem === "register"}
-              onClick={this.handleItemClick}
-            />
+
+            {user.isAuthenticated ? (
+              <Menu.Item
+                name="user"
+                active={activeItem === "user"}
+                onClick={this.handleItemClick}
+              >
+                <Icon name="user" />
+                {user.firstname}
+              </Menu.Item>
+            ) : (
+                <>
+                <Menu.Item
+                  name="login"
+                  active={activeItem === "login"}
+                  onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                  name="register"
+                  active={activeItem === "register"}
+                  onClick={this.handleItemClick}
+                />
+              </>
+            )}
           </Menu.Menu>
         </Menu>
       </div>
@@ -46,4 +63,9 @@ class AppHeader extends React.Component {
   }
 }
 
-export default withRouter(AppHeader);
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+export default withRouter(connect(mapStateToProps)(AppHeader));
