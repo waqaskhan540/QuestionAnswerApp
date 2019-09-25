2b5d0f1840016db9fb499cb3b2a433dd287d03b0
Waqas <muhammad.waqas@wearenova.co.uk>
2019-09-25 14:58:00 +0500

    code refactoring

--- client-app/src/components/questionDetail.js
+++ client-app/src/components/questionDetail.js
@@ -1,40 +1,41 @@
-import React, { Component } from "react";
-
+import React from "react";
 import { Header, Divider, Container, Segment, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-class QuestionDetail extends Component {
-  render() {
-    const { isLoading, answers, question, isUserAuthenticated } = this.props;
-    if (isLoading) return <div>Loading ...</div>;
+const QuestionDetail = ({
+  isLoading,
+  answers,
+  question,
+  isUserAuthenticated
+}) => {
+  if (isLoading) return <div>Loading ...</div>;
 
-    return (
-      <Container>
-        <Header as="h1">{question.questionText}</Header>
-        <span>{question.user.firstName}&nbsp;</span>
-        <span>{question.user.lastName}</span> - &nbsp;
-        <span>{new Date(question.dateTime).toLocaleDateString()}</span>
-        <div>
-          {isUserAuthenticated ? (
-            <Link to={`/write/${question.id}`}>
-              <Button content="Write an Answer" basic />
-            </Link>
-          ) : (
-            ""
-          )}
-        </div>
-        <Divider />
-        {answers.map(ans => (
-          <Segment key={ans.answerId}>
-            <Header as="h3">
-              {ans.user.firstName} {ans.user.lastName}
-            </Header>
-            <p>{ans.answerMarkup}</p>
-          </Segment>
-        ))}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Header as="h1">{question.questionText}</Header>
+      <span>{question.user.firstName}&nbsp;</span>
+      <span>{question.user.lastName}</span> - &nbsp;
+      <span>{new Date(question.dateTime).toLocaleDateString()}</span>
+      <div>
+        {isUserAuthenticated ? (
+          <Link to={`/write/${question.id}`}>
+            <Button content="Write an Answer" basic />
+          </Link>
+        ) : (
+          ""
+        )}
+      </div>
+      <Divider />
+      {answers.map(ans => (
+        <Segment key={ans.answerId}>
+          <Header as="h3">
+            {ans.user.firstName} {ans.user.lastName}
+          </Header>
+          <p>{ans.answerMarkup}</p>
+        </Segment>
+      ))}
+    </Container>
+  );
+};
 
 export default QuestionDetail;
